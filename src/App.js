785cb2a38fecc353a5845e72bf85699e2c1d439c
Mohsin Miah <moemia12@gmail.com>
2021-09-05 22:35:49 +0100

    Styling for content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
+import ImageListItemBar from '@material-ui/core/ImageListItemBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+import Pagination from '@material-ui/lab/Pagination';
+
 import './App.css';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  imageList: {
+    width: 500,
+    height: 450,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+  paginator: {
+    '& > *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+
+}));
+
 function App() {
+  const style = useStyles();
 
-  //Hook to initialise state
+  let totalBeers = 0;
+  // Hook to initialise state
   const [beers, setBeers] = useState(null);
+  const [filteredBeers, setFilteredBeers] = useState(null);
 
-  //Hook to retrieve information from API
+  // Hook to retrieve information from API
   useEffect(() => {
     getBeers();
 
@@ -14,21 +49,63 @@ function App() {
       const res = await fetch("https://api.punkapi.com/v2/beers");
       const data = await res.json();
 
+      totalBeers = data.length
       setBeers(data);
+      setFilteredBeers(data.slice(0, 10));
     }
   }, []);
+
+  function handlePageChange(event) {
+    const index = parseInt(event.target.innerText)
+    const offset = (index - 1) * 10;
+    setFilteredBeers(beers.slice(offset, offset + 10));
+  }
+
+  function filterByVolume() {
+    beers.filter((beer) => beer.abv > 3);
+  }
 
   //Returning data from API
   return (
     <div className="App">
 
+      <div className="header-image">
+        <img src="/images/brewdog_logo.png" />
+        <img src="/images/talentticker.png" />
+      </div>
 
-      {beers && (
+      <div className={style.paginator}>
+        <Pagination count={3} color="primary" onChange={handlePageChange}/>
+      </div>
+
+      <div className={style.root}>
+        <ImageList rowHeight={180} className={style.imageList}>
+          <ImageListItem key="Subheader" cols={3} style={{ height: 'auto' }}>
+            <ListSubheader component="div">Beers</ListSubheader>
+          </ImageListItem>
+          {filteredBeers && filteredBeers.map((beer, index) => (
+            <ImageListItem key={index}>
+              <img src={beer.image_url} alt={beer.name} />
+              <ImageListItemBar
+                title={beer.name}
+                subtitle={<span>by: {beer.name}</span>}
+                actionIcon={
+                  <IconButton aria-label={`info about ${beer.name}`} className={style.icon}>
+                    <InfoIcon />
+                  </IconButton>
+                }
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </div>
+
+      {/* {beers && (
         <div className="beers">
 
           {beers.map((beer, index) => (
 
-            <div key={index}>
+            <div className="beer" key={index}>
               <h2>{beer.name}</h2>
               <h5><text>{beer.description}</text></h5>
               <img src={beer.image_url} alt=''></img>
@@ -36,7 +113,7 @@ function App() {
           ))}
 
         </div>
-      )}
+      )} */}
 
     </div>
   );
